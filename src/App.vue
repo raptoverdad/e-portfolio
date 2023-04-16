7f3eb0fad6d507f2750c0cd70688918e608f1be2
maquinadeguerraeslaonda <contactogoodbunny@gmail.com>
2023-04-16 02:01:42 -0400

    adding some changes and files

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <div class="logo"> <img src="./assets/caraNoBackground.png" alt=""></div>
+    <div class="routerOptions">
+      <router-link to="/">Home</router-link> 
+      <router-link to="/about">About</router-link>
+    </div>
+
   </nav>
   <router-view/>
 </template>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -14,14 +18,33 @@
   text-align: center;
   color: #2c3e50;
 }
-
+.logo{
+  background-color: #000;
+}
+.routerOptions{
+  display: flex;
+  align-items: center;
+}
+.routerOptions a{
+margin: 10%;
+}
+img{
+  height: 25vh;
+  width: 25vh;
+}
 nav {
   padding: 30px;
+  display: flex;
+  background-color: aqua;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 
 nav a {
   font-weight: bold;
   color: #2c3e50;
+
 }
 
 nav a.router-link-exact-active {
